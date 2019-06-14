57bf9cebc94f50be532d7f6dbc92d448a739b98f
Coder <codyjwalker@cs.arizona.edu>
2019-06-13 20:15:41 -0700

    PythonGraphics seems to be good to go

--- PythonGraphics.py
+++ PythonGraphics.py
@@ -21,38 +21,63 @@
  " --------------------------------------------------------------------------"""
 
 
-
 import turtle
 
 
-with open("py_gui_input.txt", "r") as file:
-    NUM_BODIES = file.readline()
-    BODY_RADIUS = file.readline()
-    TIMESTEPS = file.readline()
-    print("NUM_BODIES:", NUM_BODIES, "BODY_RADIUS", BODY_RADIUS, "TIMESTEPS",
-          TIMESTEPS)
+pdb = 1
 
-file.close()
 
-"""
+bodies = []
+
+
 win = turtle.Screen()
 win.bgcolor("black")
 win.title("N-Body Gravitational Simulation")
 
 
-body = turtle.Turtle()
-body.shape("circle")
-body.color("green")
-# Dont draw path lines.
-body.penup()
-# We will move the bodies ourselves, don't let python animate for us.
-body.speed(0)
-body.goto(0, 200)
+with open("py_gui_input.txt", "r") as file:
+    # First read in how many bodies, radius of each body, & how many timesteps
+    # to run the simulation from the .txt file.
+    NUM_BODIES = int(file.readline())
+    BODY_RADIUS = int(file.readline())
+    TIMESTEPS = int(file.readline())
+
+    if (pdb):
+        print("NUM_BODIES:", NUM_BODIES, "BODY_RADIUS", BODY_RADIUS, "TIMESTEPS",
+              TIMESTEPS)
+
+    # Add a NUM_BODIES bodies to the bodies list (yes, I know...)
+    for _ in range(NUM_BODIES):
+        bodies.append(turtle.Turtle())
+
+    # Initialize each body.
+    for body in bodies:
+        body.shape("circle")
+        body.color("green")
+        # Dont draw path lines.
+        body.penup()
+        # We will move the bodies ourselves, don't let python animate for us.
+        body.speed(0)
 
 
-# To prevent everything from crashing.
-turtle.done()
+    # Keep going until all timesteps have been animated.
+    for _ in range(TIMESTEPS):
+        # Animate each body.
+        for body in bodies:
+            xpos = float(file.readline())
+            ypos = float(file.readline())
+            if (pdb): print("xpos:", xpos, "ypos:", ypos)
+            body.goto(xpos, ypos)
 
-"""
+    # To prevent everything from crashing.
+    turtle.done()
+
+# Dont forget to bring a towel!
+file.close()
+
+
+
+
+
 
 """ END PythonGraphics.py """
