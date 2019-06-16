b971731ecde77653cbe3f829da1dc5698f9cbb4d
Cody Walker <themightycoder@gmail.com>
2019-06-15 18:16:48 -0700

    Changed up how GUI will be setup, now using grid layout

--- GUI.py
+++ GUI.py
@@ -15,12 +15,72 @@
 
 from tkinter import *
 
+root = Tk()
+root.title('N-Body Gravitational Simulation')
+root.geometry("300x400")
 
-w = Tk()
-w.title('N-Body Gravitational Simulation')
-button = Button(w, text='Click Me!!!', width=20, command=w.destroy)
-button.pack()
-w.mainloop()
+
+label1 = Label(root, text = "Number of bodies:    ")
+label2 = Label(root, text = "Number of timesteps:    ")
+
+sb1 = Spinbox(root, width = 5, from_ = 1, to = 10)
+sb2 = Spinbox(root, width = 5, from_ = 1, to = 3000)
+
+cb1 = Checkbutton(root, text = "Enable Debug Print Statements")
+cb2 = Checkbutton(root, text = "Enable Graphical Output")
+
+
+
+label1.grid(row = 0, column = 0, sticky = E)
+label2.grid(row = 1, column = 0, sticky = E)
+
+sb1.grid(row = 0, column = 2, sticky = W)
+sb2.grid(row = 1, column = 2, sticky = W)
+
+cb1.grid(columnspan = 2, sticky = W)
+cb2.grid(columnspan = 2, sticky = W)
+
+
+"""
+top_frame = Frame(root)
+top_frame.pack()
+bottom_frame = Frame(root)
+bottom_frame.pack(side = BOTTOM)
+
+graphics_var = IntVar()
+Checkbutton(top_frame, text = 'Enable Graphical Output', variable =
+            graphics_var).grid(row = 0, sticky = W)
+
+debug_print_var = IntVar()
+Checkbutton(top_frame, text = 'Enable Debug Print Statements', variable =
+            debug_print_var).grid(row = 1, sticky = W)
+
+num_bodies_label = StringVar()
+num_bodies_label.set("Number of bodies:")
+label_dir = Label(bottom_frame, textvariable = num_bodies_label, height = 4)
+label_dir.pack(side = "left")
+
+num_bodies = StringVar(None)
+num_bodies_box = Spinbox(bottom_frame, textvariable = num_bodies, from_ = 1, to
+                         = 10)
+num_bodies_box.pack(side = "left")
+
+timesteps = StringVar(None)
+timesteps_box = Spinbox(bottom_frame, textvariable = timesteps, from_ = 1, to
+                        = 3000)
+timesteps_box.pack(side = "left")
+
+"""
+
+
+"""
+bottom_frame = Scale(root, from_ = 1, to = 20, orient = HORIZONTAL)
+bottom_frame.pack()
+bottom_frame = Scale(root, from_ = 1, to = 3000, orient = HORIZONTAL)
+bottom_frame.pack()
+"""
+
+mainloop()
 
 
 """ END GUI.py """
