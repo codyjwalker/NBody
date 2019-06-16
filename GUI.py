4cda4cc6ba27603719ebdb8a710cfd8f66896881
Cody Walker <themightycoder@gmail.com>
2019-06-15 20:09:15 -0700

    Added Spinboxes for body radius and body mass

--- GUI.py
+++ GUI.py
@@ -21,10 +21,21 @@ WHERE test.sh IS A SIMPLE SHELL SCRIPT
 """
 
 
+pdb = 1
+
+
 from tkinter import *
 
 
-
+"""-----------------------------------------------------------------------------
+ " Function:    run_simulation
+ " Description: Invoked upon the user pressing the 'Run Simulatin' button.
+ "              First writes all the values to a file so that the computational
+ "              programs can access them, then runs a bash script to run the
+ "              appropriate program(s) in order to run the simulation.
+ " Arguments:   None.
+ " Returns:     Nothing.
+ " --------------------------------------------------------------------------"""
 def run_simulation():
     global num_bodies_sb
     global num_timesteps_sb
@@ -38,18 +49,25 @@ def run_simulation():
         file.write("\n")
         file.write(str(num_timesteps_sb.get()))
         file.write("\n")
+        file.write(str(body_radius_sb.get()))
+        file.write("\n")
+        file.write(str(body_mass_sb.get()))
+        file.write("\n")
         file.write(str(var_debug_cb.get()))
         file.write("\n")
         file.write(str(var_graphics_cb.get()))
         file.write("\n")
 
+    if (pdb):
+        print(num_bodies_sb.get())
+        print(num_timesteps_sb.get())
+        print(body_radius_sb.get())
+        print(body_mass_sb.get())
+        print(var_debug_cb.get())
+        print(var_graphics_cb.get())
+
     # Dont forget to bring a towel!
     file.close()
-
-    print(num_bodies_sb.get())
-    print(num_timesteps_sb.get())
-    print(var_debug_cb.get())
-    print(var_graphics_cb.get())
     return
 
 
@@ -59,12 +77,19 @@ root.title('N-Body Gravitational Simulation')
 # root.geometry("300x400")
 
 
+# Labels for each of the Spinboxes.
 num_bodies_label = Label(root, text = "Number of bodies:    ")
 num_timesteps_label = Label(root, text = "Number of timesteps:    ")
+body_radius_label = Label(root, text = "Radius of each body:    ")
+body_mass_label = Label(root, text = "Mass of each body:    ")
 
-num_bodies_sb = Spinbox(root, width = 5, from_ = 1, to = 10)
-num_timesteps_sb = Spinbox(root, width = 5, from_ = 1, to = 3000)
+# The Spinboxes for selecting values for the simulation.
+num_bodies_sb = Spinbox(root, width = 12, from_ = 1, to = 10)
+num_timesteps_sb = Spinbox(root, width = 12, from_ = 1, to = 3000)
+body_radius_sb = Spinbox(root, width = 12, from_ = 1, to = 50)
+body_mass_sb = Spinbox(root, width = 12, from_ = 100000, to = 10000000000)
 
+# Checkboxes & their respective variables.
 var_debug_cb = IntVar()
 var_graphics_cb = IntVar()
 debug_cb = Checkbutton(root, text = "Enable Debug Print Statements", variable =
@@ -73,16 +98,20 @@ graphics_cb = Checkbutton(root, text = "Enable Graphical Output", variable =
                           var_graphics_cb)
 
 
+# The button the user presses to start the simulation.
 run_button = Button(root, text = "Run Simulation", width = 10, command =
                     run_simulation)
 
-
-
+# Grid layout of all items.
 num_bodies_label.grid(row = 0, column = 0, sticky = E)
 num_timesteps_label.grid(row = 1, column = 0, sticky = E)
+body_radius_label.grid(row = 2, column = 0, sticky = E)
+body_mass_label.grid(row = 3, column = 0, sticky = E)
 
 num_bodies_sb.grid(row = 0, column = 1, sticky = W)
 num_timesteps_sb.grid(row = 1, column = 1, sticky = W)
+body_radius_sb.grid(row = 2, column = 1, sticky = W)
+body_mass_sb.grid(row = 3, column = 1, sticky = W)
 
 debug_cb.grid(columnspan = 2, sticky = W)
 graphics_cb.grid(columnspan = 2, sticky = W)
@@ -90,47 +119,7 @@ graphics_cb.grid(columnspan = 2, sticky = W)
 run_button.grid(columnspan = 2)
 
 
-
-
-"""
-top_frame = Frame(root)
-top_frame.pack()
-bottom_frame = Frame(root)
-bottom_frame.pack(side = BOTTOM)
-
-graphics_var = IntVar()
-Checkbutton(top_frame, text = 'Enable Graphical Output', variable =
-            graphics_var).grid(row = 0, sticky = W)
-
-debug_print_var = IntVar()
-Checkbutton(top_frame, text = 'Enable Debug Print Statements', variable =
-            debug_print_var).grid(row = 1, sticky = W)
-
-num_bodies_label = StringVar()
-num_bodies_label.set("Number of bodies:")
-label_dir = Label(bottom_frame, textvariable = num_bodies_label, height = 4)
-label_dir.pack(side = "left")
-
-num_bodies = StringVar(None)
-num_bodies_box = Spinbox(bottom_frame, textvariable = num_bodies, from_ = 1, to
-                         = 10)
-num_bodies_box.pack(side = "left")
-
-timesteps = StringVar(None)
-timesteps_box = Spinbox(bottom_frame, textvariable = timesteps, from_ = 1, to
-                        = 3000)
-timesteps_box.pack(side = "left")
-
-"""
-
-
-"""
-bottom_frame = Scale(root, from_ = 1, to = 20, orient = HORIZONTAL)
-bottom_frame.pack()
-bottom_frame = Scale(root, from_ = 1, to = 3000, orient = HORIZONTAL)
-bottom_frame.pack()
-"""
-
+# The main loop.
 mainloop()
 
 
